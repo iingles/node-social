bbefb86a60ff4f13fbff01204f7a786ee3b64634
isaac <isaac.ingles@gmail.com>
2020-02-17 08:11:51 -0700

    added styles to 404 page

--- frontend/src/views/404.vue
+++ frontend/src/views/404.vue
@@ -1,6 +1,12 @@
 <template>
-    <v-content>
-        <h1>Not Found</h1>
-        <p>Whatever you were looking for, it's not here.</p>
-    </v-content>
+    <v-container>
+        <v-row>
+            <v-spacer></v-spacer>
+            <v-col cols="12" xs="12" lg="6">
+                <h1>Not Found</h1>
+                <p>Whatever you were looking for, it's not here.</p>
+            </v-col>
+            <v-spacer></v-spacer>
+        </v-row>
+    </v-container>
 </template>
