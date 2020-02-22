d108b24de1ab0de09c1d0a833f0c72afb80f8307
isaac <isaac.ingles@gmail.com>
2020-02-22 16:09:22 -0700

    got rid of console.log

--- backend/app.js
+++ backend/app.js
@@ -21,8 +21,6 @@ const app = express()
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-rxesh.mongodb.net/${process.env.MONGO_DEFAULT_DB}`
 const PORT = process.env.PORT || 3000
 
-console.log(MONGODB_URI)
-
 //Uploading files with Multer
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
